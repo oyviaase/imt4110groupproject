e5158444022926a208c3a66f0947b7bd2b949adf
Øyvind Jensen <oyvindjens02@gmail.com>
2017-10-25 16:31:05 +0200

    Initial passwordhasher. Will has passwords placed in passwords.txt and the outputted hashed passwords will be output to hashedpws.txt in the format which Johntheripper supports.

---
+++ hashmaker.py
@@ -0,0 +1,12 @@
+import hashlib
+
+with open('passwords.txt', 'r') as pwfile, open('hashedpws.txt', 'w') as outfile:
+	for password in pwfile:
+		pwhash = hashlib.md5(password)
+		pwhash = pwhash.hexdigest()
+		#pwhash.strip()
+		password = password + ':' + pwhash
+		password = password.replace('\n', '')
+		print password
+		outfile.write(password)
+		outfile.write('\n')
